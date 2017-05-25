017339dfaa542f5a7c7bbb307cffdb3781858d9d
Diogo Gomes <diogo.gomes@b2wdigital.com>
2017-05-25 15:20:56 -0300

    Add RateLimiting plugin

--- plugin.go
+++ plugin.go
@@ -16,3 +16,22 @@ type RequestTransformer struct {
 	AppendQuerystring  string `url:"config.append.querystring,omitempty"`
 	AppendBody         string `url:"config.append.body,omitempty"`
 }
+
+type RateLimiting struct {
+	Name                string `url:"name"`
+	ConsumerId          string `url:"consumer_id,omitempty"`
+	ConfigSecond        string `url:"config.second,omitempty"`
+	ConfigMinute        string `url:"config.minute,omitempty"`
+	ConfigHour          string `url:"config.hour,omitempty"`
+	ConfigDay           string `url:"config.day,omitempty"`
+	ConfigMonth         string `url:"config.month,omitempty"`
+	ConfigYear          string `url:"config.year,omitempty"`
+	ConfigLimitBy       string `url:"config.limit_by,omitempty"`
+	ConfigPolicy        string `url:"config.policy,omitempty"`
+	ConfigFaultTolerant string `url:"config.fault_tolerant,omitempty"`
+	ConfigRedisHost     string `url:"config.redis_host,omitempty"`
+	ConfigRedisPort     string `url:"config.redis_port,omitempty"`
+	ConfigRedisPassword string `url:"config.redis_password,omitempty"`
+	ConfigRedisTimeout  string `url:"config.redis_timeout,omitempty"`
+	ConfigRedisDatabase string `url:"config.redis_database,omitempty"`
+}
